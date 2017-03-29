d20a2aead58d9415f5c538790ee07abdfea16a07
Harry Hu <hurung@gmail.com>
2017-03-29 14:42:52 +1100

    Create server project

--- WeiERP/WeiERPClient/WebContent/app/components/order/order.tsx
+++ WeiERP/WeiERPClient/WebContent/app/components/order/order.tsx
@@ -3,6 +3,7 @@ import {State} from '../../reducers/reducerTypes';
 import {Link} from 'react-router'
 import {Order, DataList,Error} from '../../models/modelTypes'
 import Table from '../elements/table'
+import EditableSelect from '../elements/editableSelect'
 import Invoice from '../order/invoice'
 import * as OrderActions from '../../actions/orderActions'
 
@@ -60,6 +61,8 @@ export default class OrderPage extends React.Component<OrderPageProps,{}>{
 		    		<div className="row">
 		    		  <div className="col-md-12 col-sm-12 col-xs-12">
 			            <div className="x_panel">
+			              <EditableSelect optionList={[{value:'111'},{value:'111'},{value:'111'},{value:'111'}]} valueName="value" textName="value" name="test"/>
+				              
 			              <div className="x_title">
 			                <h2>Table design <small>Custom design</small></h2>
 			                <ul className="nav navbar-right panel_toolbox">
@@ -79,7 +82,6 @@ export default class OrderPage extends React.Component<OrderPageProps,{}>{
 			                </ul>
 			                <div className="clearfix"></div>
 			              </div>
-			
 			              <div className="x_content">
 			
 			                <p>Add class <code>bulk_action</code> to table for bulk actions options on row select</p>
