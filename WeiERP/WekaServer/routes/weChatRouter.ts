8df30e3f572ba525bd9dd2f478d0c5ef05cee3ce
Harry.Hu <hurung@gmail.com>
2017-06-06 12:38:52 +1000

    change chat router

--- WeiERP/WekaServer/routes/weChatRouter.ts
+++ WeiERP/WekaServer/routes/weChatRouter.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 import WeChatController from "../controller/weChatController";
 import Logger from '../server/logger';
 import ProtectedRouter from "./protectedRouter";
+import ChatController from "../controller/chatController";
 
 const logger = new Logger("WeChatRouter");
 
@@ -10,7 +11,9 @@ export default class WeChatRouter{
   constructor() {
     let router = express.Router();
     let weChatController = new WeChatController();
+    let chatController = new ChatController();
     router.get('/', weChatController.validate.bind(weChatController));
+    router.post('/', chatController.chat.bind(chatController));
     logger.info('register wechat router');
     return router;
   }
