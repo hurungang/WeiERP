2e866684b79d126fb9889ff41d46454a6054e0a2
Harry.Hu <hurung@gmail.com>
2017-06-13 22:59:30 +1000

    add out put to footer for safari debug

--- WeiERP/WekaClient/WebContent/app/components/layout/footer.tsx
+++ WeiERP/WekaClient/WebContent/app/components/layout/footer.tsx
@@ -11,6 +11,22 @@ export default class Footer extends React.Component<FooterProps,{}>{
 	          {footerText}
 	        </div>
 	        <div className="clearfix"></div>
+	        <div id="debugDiv"></div>
         </footer>
     );
 }}
+
+
+  if (typeof console  != "undefined") 
+    if (typeof console.log != 'undefined')
+        console['olog'] = console.log;
+    else
+        console['olog'] = function() {};
+
+console.log = function(message) {
+    console['olog'](message);
+    let element = document.getElementById('debugDiv');
+		element.innerHTML = element.innerHTML + '<p>' + message + '</p>';
+};
+
+console.error = console.debug = console.info =  console.log
