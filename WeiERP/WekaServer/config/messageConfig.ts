571d42381c07f0e233e3329a4970b02837da2b9d
Harry.Hu <hurung@gmail.com>
2017-06-13 19:59:31 +1000

    debug

--- WeiERP/WekaServer/config/messageConfig.ts
+++ WeiERP/WekaServer/config/messageConfig.ts
@@ -11,4 +11,4 @@ export const ORDER_REPLY = (order:IOrderModel) => `你的订单已生成,
 订单内容:
   ${order.orderItems.map((orderItem)=>`${orderItem.product.productName} - ${orderItem.productQuantity}
   `)}
-点击查看:${OAUTH_CLIENT.getAuthorizeURL(serverConfig.SERVER_DOMAIN + '/web/#/order/'+order.id, DataUtil.encrypt(order.user.referenceID), 'snsapi_base')}`;
+点击查看:${OAUTH_CLIENT.getAuthorizeURL(serverConfig.SERVER_DOMAIN + '/web/#/order/'+order.id+"?token="+DataUtil.encrypt(order.user.referenceID), DataUtil.encrypt(order.user.referenceID), 'snsapi_base')}`;
